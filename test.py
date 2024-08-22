8fe5b4cb2e6cfe83c2c3be3164eb738c9de30db2
Su Wai Phyoe <beatriz@Sus-MacBook-Pro.local>
2024-08-22 09:25:53 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,58 @@
+print ("im good")
+number = input ("Give me a number?")
+# number is a variable and input is to ask data from the user
+print ("number")
+number_of_year = input("input a year:")
+name= input ("What is your name?")
+print ("Hello"+ name)
+""
+int_var = 3
+float_var= 23.8
+string_var= " susu"
+
+print (type (int_var))
+print(type(float_var))
+print(type(string_var))
+""
+name= input("Name:")
+print (name)
+name= name+ "cool"
+print (name)
+""""""
+result= 18/8
+print (result)
+print(type(result))
+rounded_result=int(result)
+print(rounded_result)
+number1= int(input("Give me a number"))
+number2= int(input("Give me a number"))
+print(number1 + number2)
+
+
+print (3+2)
+print(3*2)
+print(3//2)
+num= 30
+print("This is a number + str(num)") # covert a variable into string line
+print("This is a number",num)
+print("This is a number {num}")
+num2= 1/3
+print(num2)
+print("This is the new num{ num2:.5f}")
+
+name= input("What is your name?")
+print("name:su wai phyoe")
+year= input("what is the year")
+print( name+"is drinking coffee in the early morning. She also likes to watch youtube alot ")
+
+days= input("how many days?:")
+
+seconds= input("how many seconds?:")
+
+hm=int(input(" How many times a week do you eat at the student cafeteria?:"))
+price= float(input("The price of a typical student lunch?:"))
+gg= float(input(" How much money do you spend on groceries in a week?:"))
+print(" Average food expenditure:")
+print (f"Daily :{(hm*price+gg)/7}")
+print(f"weekly{hm*price+gg}")
+
