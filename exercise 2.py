2d2c0d64557162511f9603ec847e84a9cc10ce72
suwaiphyoe-beatriz <beatrizisme4@gmail.com>
2024-08-23 21:47:39 +0300

    h

--- exercise 2.py
+++ exercise 2.py
@@ -1,1 +1,7 @@
-print("somethings")
+talent = float(input("Enter talents: "))
+pound = float(input("Enter pounds: "))
+lot = float(input("Enter lots: "))
+mass = ((talent * 20 + pound) * 32 + lot) * 13.3
+mass_kg = int(mass // 1000)
+mass_gr = mass % 1000
+print("The weight in modern units:", mass_kg, f"kilograms and {mass_gr:.2f} grams")
